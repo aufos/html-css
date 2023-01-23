7fad9f931b803dbb428a93f9f299bd57c676dd9a
aufos <gzyue5@outlook.com>
2023-01-23 15:47:42 +0100

    js.4

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 
-var username = document.getElementById("username").value;
-var password = document.getElementById("password").value;
-var repeatPassword = document.getElementById("repeatPassword").value;
-var fullname = document.getElementById("fullName").value;
-var country = document.getElementById("country").value;
-var zipcode = document.getElementById("zipcode").value;
-var emai = email.getElementById("email").value;
 
 function validate(){
+  
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  var repeatPassword = document.getElementById("repeatPassword").value;
+  var fullname = document.getElementById("fullName").value;
+  var country = document.getElementById("country").value;
+  var zipcode = document.getElementById("zipcode").value;
+  var emai = email.getElementById("email").value;
+
     let x = document.forms["myForm"]["fname"].value;
   if (x == "") {
     alert("Name must be filled out");
