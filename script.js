e8179a3fc3c8949cf79507f259420e85c2e6254d
aufos <gzyue5@outlook.com>
2023-01-23 15:57:03 +0100

    js.5

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 function validate(){
-  
+
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
   var repeatPassword = document.getElementById("repeatPassword").value;
@@ -10,11 +10,15 @@ function validate(){
   var zipcode = document.getElementById("zipcode").value;
   var emai = email.getElementById("email").value;
 
-    let x = document.forms["myForm"]["fname"].value;
-  if (x == "") {
-    alert("Name must be filled out");
-    return false;
-  }
+  var username_err = document.getElementById("username_error").value;
+  var password_err = document.getElementById("password_error").value;
+  var repeatPassword_err = document.getElementById("repeatPassword_error").value;
+  var fullname_err = document.getElementById("fullName_error").value;
+  var country_err = document.getElementById("country_error").value;
+  var zipcode_err = document.getElementById("zipcode_error").value;
+  var email_err = email.getElementById("email_error").value;
+
+
    
 
     alert("Welcme");
